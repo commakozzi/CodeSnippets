aaabbcb923ac2b4cc4308d62daee356e1e69aec7
commakozzi <commakozzi1@gmail.com>
2016-05-17 13:28:17 -0400

    Updated getButtons

--- getButtons.java
+++ getButtons.java
@@ -4,6 +4,9 @@ mCalcButtonViewGroup = (ViewGroup) findViewById(R.id.calcScreen_fragment);
 //Get buttons from calcScreen_fragment:
 getButtons(mCalcButtonViewGroup);
 
+private ViewGroup mCalcButtonViewGroup;
+private List<Button> mButtons = new ArrayList<>();
+
 //Method for getting buttons:
 private void getButtons(ViewGroup v) {
     for (int i = 0; i < v.getChildCount(); i++) {
@@ -13,5 +16,18 @@ private void getButtons(ViewGroup v) {
         } else if (child instanceof Button) {
             mButtons.add((Button) child);
         }
+    }
+}
+
+@Override
+public void onClick(View v) {
+    String calcScreenText = calcScreen.getText().toString();
+    switch(v.getId()) {
+        case R.id.btnOne:
+            if(calcScreenText == null) {
+                calcScreen.setText("1");
+            } else {
+                calcScreen.setText(calcScreenText + "1");
+            }
     }
 }
